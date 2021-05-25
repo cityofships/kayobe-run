e5cf31412bdf8e0402ceaa5c0feb5f65e457c78a
Piotr Parczewski <piotr@stackhpc.com>
2021-05-25 22:35:39 +0200

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -e
 set -o pipefail
+
+echo ">>> Debugging"
+echo ""
+pwd
+ls -lha
+env | sort
+
 echo ">>> Running command"
 echo ""
 bash -c "set -e;  set -o pipefail; $1"
